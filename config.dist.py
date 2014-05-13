15e9bbcd03cbc35a2ed1c003e461c38dc272b721
Marian Steinbach <marian@sendung.de>
2014-05-13 09:37:13 +0200

    Yes, bad bug :)

---
+++ config.dist.py
@@ -0,0 +1,27 @@
+# encoding: utf8
+
+"""
+Configuration example. Copy this to
+config.py and edit according to your needs
+"""
+
+# MySQL host
+DB_HOST = "localhost"
+
+# MySQL credentials
+DB_USER = "root"
+DB_PASS = ""
+
+# MySQL database name
+DB_NAME = "bfs-gamma-archiver"
+
+# your MySQL database engine of choice
+# (leave "MyISAM" when in doubt)
+DB_ENGINE = "MyISAM"
+
+# Entry point URL for bfs download folder
+BFS_URL = "http://odlinfo.bfs.de/daten/"
+
+# bfs credentials
+FTP_USER = "your_user_name_here"
+FTP_PASS = ""
